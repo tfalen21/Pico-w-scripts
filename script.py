933ab2c01ba0c483cc2c0446e5abff8b88943957
tfalen <tuckerfalen21@gmail.com>
2025-02-06 22:46:06 -0600

    fixed script, now working

--- script.py
+++ script.py
@@ -3,25 +3,51 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-#This script requires you to install CircuitPython onto your Pico and the adafruit_hid library.
-
 # Initialize keyboard
 keyboard = Keyboard(usb_hid.devices)
 
 # Delay before execution (for safety)
 time.sleep(3)
 
-# Example payload: Open Notepad and type a message (Windows)
-keyboard.send(Keycode.WINDOWS, Keycode.R)  # Open Run dialog
+# Open Run dialog (Win + R)
+keyboard.press(Keycode.WINDOWS, Keycode.R)
+keyboard.release_all()
 time.sleep(0.5)
-keyboard.send(Keycode.BACKSPACE)  # Ensure clean input
+
+# Ensure clean input
+keyboard.send(Keycode.BACKSPACE)
 time.sleep(0.2)
-keyboard.send(Keycode.N, Keycode.O, Keycode.T, Keycode.E, Keycode.P, Keycode.A, Keycode.D)
+
+# Type "notepad" correctly
+notepad_text = [Keycode.N, Keycode.O, Keycode.T, Keycode.E, Keycode.P, Keycode.A, Keycode.D]
+
+for key in notepad_text:
+    keyboard.press(key)
+    time.sleep(0.05)
+    keyboard.release_all()
+
 time.sleep(0.5)
-keyboard.send(Keycode.ENTER)  # Open Notepad
-time.sleep(1)
-keyboard.send(Keycode.H, Keycode.E, Keycode.L, Keycode.L, Keycode.O)
-time.sleep(0.2)
-keyboard.send(Keycode.SPACE)
-keyboard.send(Keycode.W, Keycode.O, Keycode.R, Keycode.L, Keycode.D, Keycode.EXCLAMATION)
+
+# Press Enter to open Notepad
+keyboard.send(Keycode.ENTER)
+time.sleep(2)  # Extended delay to allow Notepad to fully open
+
+# Type "Hello World!" into Notepad
+message = "Hello World!"  # Using a string instead of keycodes
+
+for char in message:
+    if char.isupper():
+        keyboard.press(Keycode.SHIFT, getattr(Keycode, char.upper(), Keycode.SPACE))
+        keyboard.release_all()
+    elif char == " ":
+        keyboard.send(Keycode.SPACE)
+    elif char == "!":
+        keyboard.press(Keycode.SHIFT, Keycode.ONE)
+        keyboard.release_all()
+    else:
+        keyboard.send(getattr(Keycode, char.upper(), Keycode.SPACE))
+    
+    time.sleep(0.05)  # Delay to prevent skipping characters
+
+# Press Enter at the end
 keyboard.send(Keycode.ENTER)
